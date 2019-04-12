5824bf3e9d9f856127e2a59e04136518947dc613
Shelley Arnold <shelle.kay@gmail.com>
2019-04-12 12:52:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Cookout from practice
---
+++ js/main.js
@@ -0,0 +1,67 @@
+// Your task is to iterate over the array of foods and invoke the function for each item so that the cookedFood array contains all of the items after they are cooked.
+
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+const cookedFood = [];
+
+
+// function grill (currentObject) {
+//     currentObject.cooked = true;
+//     cookedFood.push(currentObject);
+// };
+
+// grill(steak);
+// grill(corn);
+// console.log(cookedFood);
+
+
+function grill(foodarray) {
+    for (let i = 0; i < foodarray.length; i++) {
+        if (foodarray[i].cooked === true) {
+            cookedFood.push(foodarray[i]);
+        }
+    }
+}
+
+grill(foods);
+console.log("all food", foods);
+console.log("cooked food", cookedFood);
+
+
+// Splice
+// function grilling(i) {
+//     i.cooked = true;
+//     cookedFood.push(i);
+//     for (var j = 0; j <= foods.length; j++) {
+//         if (foods[j] === i)
+//         foods.splice(i, 1);
+//     }
+// }
+// grilling(hamburger);
+// console.log(cookedFood)
